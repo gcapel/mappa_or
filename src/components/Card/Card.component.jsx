f49b52907cb9e9f101a5d11c0bb3e5de4250f92d
gcapel <Borgo1966>
2022-05-02 21:32:13 +0200

    prova3

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Card.styles.scss";
 
-const Card = () => <div className="card" >PROVA</div>;
+const Card = (props) => <div className="card" >{props.testo}</div>;
 
 export default Card;
