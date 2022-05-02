fef6b0f1059d4b39e9e151da292362f0e9a8302c
gcapel <Borgo1966>
2022-05-02 21:15:03 +0200

    prova2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from './components/Card/Card.component';
 export default function App() {
     return (
         <div>
-            <Card testo='CARTELLA PRINCIPALE' />
+            <Card />
         </div>
     );
 }
--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Card.styles.scss";
 
-const Card = (props) => <div className='card' >{props.testo}</div>;
+const Card = () => <div className="card" >PROVA</div>;
 
 export default Card;
